c1def13eef66605b43ef1bddd1190d1bc8838d41
PHANTOMGOD <achharyavedant@gmail.com>
2023-11-07 01:36:58 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import Footer from "@/components/Footer";
 import "./global.css";
 import Navbar from "@/components/Navbar";
 export const metadata = {
-  title: "Owaisibble",
-  description: "Get Discover The more authentic & aesthetic projects",
-  author: "Muhammad Owais",
+  title: "FLEXIBBLE",
+  description: "A Platform where UI/UX designers or Graphic Designers get to showcase their Art In Various Different Categories With Their Social Links So that they can get customers by marketing themselves on this platform",
+  author: "PHANTOMGOD2OP",
 };
 
 export default function RootLayout({
