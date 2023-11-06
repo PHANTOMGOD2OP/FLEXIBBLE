850ebe2b67adcf87feade6e049476fbafd15edec
PHANTOMGOD <achharyavedant@gmail.com>
2023-11-07 02:29:34 +0530

    Update session.ts

--- lib/session.ts
+++ lib/session.ts
@@ -11,7 +11,7 @@ export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   jwt: {
